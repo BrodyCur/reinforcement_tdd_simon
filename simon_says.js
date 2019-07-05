ea1a758b1128485d891b880f4378125da67f403f
Brody Currie <brodycurrie@gmail.com>
2019-07-05 16:47:11 -0400

    completed reinforcement

--- simon_says.js
+++ simon_says.js
@@ -1,1 +1,25 @@
+const echo = (word) => {
+  return word;
+};
+
+const shout = (word) => {
+  let capitalizeWord = word.toUpperCase();
+  return capitalizeWord;
+};
+
+const repeat = (str, int) => {
+  let repeatedStr = (str + ' ').repeat(int)
+  return repeatedStr.trim();
+};
+
+const startOfWord = (string, int) => {
+  let sub = string.substring(0, int);
+  return sub;
+};
+
+const firstWord = (str) => {
+  let strSplit = str.split(' ');
+  return strSplit[0];
+};
+
 module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
